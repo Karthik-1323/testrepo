aa2979b36b7ef9048cd9b7632e5b7efe2fe1cc9d
admin <admin@DESKTOP-C4RIFI2>
2024-12-25 18:27:12 +0530

    demo

---
+++ karthikap/src/javapractice/Varia.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Varia {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ar[]= new int ar[5];
+	      
+	      for(int i = 0; i<=4; i++){
+	    	  
+	        ar[i] = sc.nextInt();
+
+	      
+	      }
+
+	}
+
+}
